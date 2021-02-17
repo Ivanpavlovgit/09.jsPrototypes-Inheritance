0bb06da5027e6135e6072e35928d6adcc3db6a9e
IvanP <ivanstpv@gmail.com>
2021-02-17 14:38:05 +0200

    configurable works for value and enumerable

--- src/lab/theBoard.js
+++ src/lab/theBoard.js
@@ -1,24 +1,19 @@
+'use strict'
 const myOb = {};
 
-
-Object.defineProperty(myOb, '_name', {value: 'George', enumerable: false, writable: true})
-
-
-Object.defineProperty(myOb, 'name', {
-
-    get() {
-        return this._name;
-    },
-    set(value) {
-        this._name = value;
-    }, enumerable: true
+Object.defineProperty(myOb,'name',{
+    value: 'John',
+    writable:true,
+    enumerable:true,
+    configurable:false
 });
-console.log('Before ->', myOb.name);
-myOb.name = 'Peter';
-console.log('After ->', myOb.name);
 console.log(myOb);
-for (const key in myOb) {
-    let counter = 1;
-    console.log(counter, key, myOb[key]);
-}
-console.log(Object.getOwnPropertyNames(myOb));
+//delete myOb.name;
+console.log(myOb);
+Object.defineProperty(myOb,'name',{
+    value:'can change'
+});
+
+Object.defineProperty(myOb,'name',{
+    configurable:true
+});
