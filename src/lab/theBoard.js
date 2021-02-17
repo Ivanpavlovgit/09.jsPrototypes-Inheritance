2cc577f421b210ad104e46cfedc1d0e24991d899
IvanP <ivanstpv@gmail.com>
2021-02-17 14:13:09 +0200

    definePropery on the board

--- src/lab/theBoard.js
+++ src/lab/theBoard.js
@@ -1,19 +1,18 @@
-const myObj = {
-    name: 'Pesho',
-    age: 21
-};
-console.log(Object.getOwnPropertyDescriptor(myObj, 'name'));
-Object.defineProperty(myObj, 'lastName', {
-    value: 'Peshev',
-    writable: true,
-    enumerable: true,
-    configurable: true
-})
-console.log(Object.getOwnPropertyDescriptor(myObj, 'age'));
-console.log(myObj);
-for (const myObjKey in myObj) {
-    console.log(myObjKey);
-}//logs the keys
-for (const myObjKey in myObj) {
-    console.log(myObjKey,myObj[myObjKey]);
-}//logs the keys and values
+const myCollection = {};
+
+Object.defineProperty(myCollection, 'size', {
+    enumerable: false,
+    get: function () {
+        return Object.keys(this).length;
+    }
+});
+
+myCollection['Jan'] = '+359-321-553-000';
+myCollection['Clod'] = '+359-321-553-001';
+myCollection['Van'] = '+359-321-553-222';
+myCollection['Damme'] = '+359-321-553-333';
+console.log(myCollection.size);
+for (let key in myCollection) {
+    console.log(key,myCollection[key]);
+}
+console.log(Object.getOwnPropertyNames(myCollection));
