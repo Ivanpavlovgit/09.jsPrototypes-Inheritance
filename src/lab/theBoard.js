3c33cc37f5ae2aefbb6fe388140a6f0c9f425728
IvanP <ivanstpv@gmail.com>
2021-02-17 16:25:06 +0200

    freeze -Prevents the modification of existing property attributes and values, and prevents the addition of new properties. and seal-Prevents the modification of existing property attributes and values, and prevents the addition of new properties.

--- src/lab/theBoard.js
+++ src/lab/theBoard.js
@@ -1,19 +1,21 @@
-'use strict'
-const myOb = {};
+const myCollection = {};
 
-Object.defineProperty(myOb,'name',{
-    value: 'John',
-    writable:true,
-    enumerable:true,
-    configurable:false
-});
-console.log(myOb);
-delete myOb.name;
-console.log(myOb);
-Object.defineProperty(myOb,'name',{
-    value:'can change'
+Object.defineProperty(myCollection, 'size', {
+    enumerable: false,
+    get: function () {
+        return Object.keys(this).length;
+    }
 });
 
-Object.defineProperty(myOb,'name',{
-    configurable:true
-});
+myCollection['Jan'] = '+359-321-553-000';
+myCollection['Clod'] = '+359-321-553-001';
+myCollection['Van'] = '+359-321-553-222';
+myCollection['Damme'] = '+359-321-553-333';
+console.log(Object.getOwnPropertyNames(myCollection));
+console.log(myCollection['Jan']);
+Object.freeze(myCollection);//makes all properties writable:false
+myCollection['Jan']='421421';
+console.log(myCollection['Jan']);
+console.log(Object.getOwnPropertyDescriptor(myCollection,'Jan'));
+Object.seal(myCollection);//makes all properties configurable:false
+console.log(Object.getOwnPropertyDescriptor(myCollection,'Jan'));
