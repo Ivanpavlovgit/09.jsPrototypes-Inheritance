5a66faa33b1f7b38fbb2f4d7f5495f7328cbdb63
IvanP <ivanstpv@gmail.com>
2021-02-18 09:33:50 +0200

    protoypes

--- src/lab/01.Person.js
+++ src/lab/01.Person.js
@@ -1,18 +1,14 @@
 function Person(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
-    Object.defineProperty(this, 'fullName', {
-        enumerable: true,
-        get: function () {
-            return `${this.firstName} ${this.lastName}`
-        },
-        set: function (value) {
-            [this.firstName, this.lastName] = value.split(' ');
-        }
-    });
 }
-const me=new Person('terminator','merminator');
-console.log(me);
-console.log(me.firstName);
-console.log(me.lastName);
-console.log(me.fullName);
+
+Person.prototype.write = function (message) {
+    console.log(`${this.firstName} logs  ${message}`);
+}
+const foo = new Person('fname', 'lname');
+console.log(foo.hasOwnProperty('write'));
+console.log(foo instanceof Person);
+console.log(Person.hasOwnProperty('write'));
+console.log(Person.prototype.hasOwnProperty('write'));
+foo.write('msaj');
