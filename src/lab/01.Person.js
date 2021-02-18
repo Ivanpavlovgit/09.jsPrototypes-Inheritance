c8f706ffc75a77d0e8afd293fe2d8b8b66314695
IvanP <ivanstpv@gmail.com>
2021-02-18 10:45:47 +0200

    prototypes examples

--- src/lab/01.Person.js
+++ src/lab/01.Person.js
@@ -1,14 +1,27 @@
-function Person(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
+function Person(name){
+    this.name=name;
+}
+Person.prototype.sayHi=function (){
+    console.log(`${this.name} says hi!`);
+}
+function Employee(name,salary){
+    Person.call(this,name);
+    this.salary=salary;
 }
 
-Person.prototype.write = function (message) {
-    console.log(`${this.firstName} logs  ${message}`);
+
+//~~~~~~~~~~~~~Inherit with Object.create(class.class/obj.prototype)
+//if inherited with just class.class/obj.prototype it copies the reference and after change the
+// child prototype will change the parent type which is VERY BAD~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Employee.prototype=Object.create(Person.prototype);
+
+
+
+Employee.prototype.collectSalary=function (){
+    console.log(`${this.name} collected ${this.salary}`);
 }
-const foo = new Person('fname', 'lname');
-console.log(foo.hasOwnProperty('write'));
-console.log(foo instanceof Person);
-console.log(Person.hasOwnProperty('write'));
-console.log(Person.prototype.hasOwnProperty('write'));
-foo.write('msaj');
+
+const myEmployYee=new Employee('Slave','50lv');
+console.log(myEmployYee);
+myEmployYee.sayHi();
+myEmployYee.collectSalary();
